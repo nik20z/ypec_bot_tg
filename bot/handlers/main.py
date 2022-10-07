a6c88ae5386dcf537546409e3a53d2edb3437211
Nikita <62090150+nik20z@users.noreply.github.com>
2022-10-07 21:46:49 +0300

    Add files via upload

--- bot/handlers/main.py
+++ bot/handlers/main.py
@@ -7,8 +7,8 @@ from bot.handlers.user import register_user_handlers
 
 def register_all_handlers(dp: Dispatcher) -> None:
     handlers = (
-        register_user_handlers,
         register_admin_handlers,
+        register_user_handlers,
         register_other_handlers,
     )
     for handler in handlers:
