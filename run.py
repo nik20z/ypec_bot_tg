f5d2f840328ea5e1189c359326089634f4122a34
Nikita <62090150+nik20z@users.noreply.github.com>
2022-10-18 01:12:31 +0300

    Add files via upload
    Signed-off-by: Nikita <62090150+nik20z@users.noreply.github.com>

--- run.py
+++ run.py
@@ -1,4 +1,23 @@
+from loguru import logger
 from bot import start_bot
 
+
 if __name__ == '__main__':
-    start_bot()
+    logger.add("bot/log/error.log",
+               format="{time:HH:mm:ss} {level} {message}",
+               level="ERROR",
+               rotation="00:00",
+               compression="zip",
+               serialize=False,
+               enqueue=True)
+
+    logger.add("bot/log/info.log",
+               format="{time:HH:mm:ss} {level} {module} {function} {message}",
+               level="INFO",
+               rotation="00:00",
+               compression="zip",
+               serialize=False,
+               enqueue=True)
+
+    with logger.catch():
+        start_bot()
