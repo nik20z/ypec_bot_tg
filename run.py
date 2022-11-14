774f4f17b8b39dbd259ef8e678cef06b2e85887e
Nikita <62090150+nik20z@users.noreply.github.com>
2022-11-14 13:14:12 +0300

    Update run.py
    Signed-off-by: Nikita <62090150+nik20z@users.noreply.github.com>

--- run.py
+++ run.py
@@ -3,14 +3,6 @@ from bot import start_bot
 
 
 if __name__ == '__main__':
-    logger.add("bot/log/error.log",
-               format="{time:HH:mm:ss} {level} {message}",
-               level="ERROR",
-               rotation="00:00",
-               compression="zip",
-               serialize=False,
-               enqueue=True)
-
     logger.add("bot/log/info.log",
                format="{time:HH:mm:ss} {level} {module} {function} {message}",
                level="INFO",
