33373713e711e952f4fe45e4df50d37421528187
Nikita <62090150+nik20z@users.noreply.github.com>
2022-11-16 17:27:18 +0300

    Update env.py
    Signed-off-by: Nikita <62090150+nik20z@users.noreply.github.com>

--- bot/misc/env.py
+++ bot/misc/env.py
@@ -2,8 +2,8 @@ from os import environ
 from typing import Final
 
 
-class TgKeys:
-    TOKEN: Final = environ.get('TOKEN', '2070598588:123456789')
+class Keys:
+    TG_TOKEN: Final = environ.get('TG_TOKEN', '2070598588:123456789')
 
 
 class DataBase:
@@ -22,13 +22,17 @@ class Qiwi:
 class Donate:
     TINKOFF: Final = environ.get('TINKOFF', "https://www.tinkoff.ru/cf/123456789")
     QIWI: Final = environ.get('QIWI', "https://qiwi.com/p/123456789")
-    SBERBANK: Final = environ.get('SBERBANK', "https://www.sberbank.ru/ru/person/dl/jc?linkname=123456789")
-    YOOMONEY: Final = environ.get('YOOMONEY', "https://sobe.ru/na/123456789")
+    SBERBANK: Final = environ.get('SBERBANK', "https://www.sberbank.ru/ru/person/dl/123456789")
+    YOOMONEY: Final = environ.get('YOOMONEY', "https://sobe.ru/na/B2W2x1U0o2L6")
+    BOOSTY: Final = environ.get('BOOSTY', "https://boosty.to/ypec_bot")
     BITCOIN: Final = environ.get('BTC', "123456789")
     ETHERIUM: Final = environ.get('ETH', "123456789")
-    USDT_TRON: Final = environ.get('USDT_TRON', "123456789")
 
 
 class Communicate:
-    VK: Final = environ.get('VK', "https://vk.com/id264311526")
+    TG_BOT: Final = environ.get('TG_BOT', "https://t.me/ypec_bot")
+    VK_BOT: Final = environ.get('VK_BOT', "https://vk.com/ypec_bot")
+    DEVELOPER: Final = environ.get('DEVELOPER', "https://vk.com/id264311526")
     INSTAGRAM: Final = environ.get('INSTAGRAM', "https://www.instagram.com/your.oldfr1end")
+    OFFICIAL_SITE: Final = environ.get('OFFICIAL_SITE', "https://www.ypec.ru/")
+    OFFICIAL_VK_GROUP: Final = environ.get('OFFICIAL_VK_GROUP', "https://vk.com/ypecnews")
